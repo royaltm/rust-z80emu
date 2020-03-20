3c639b445f6db7eeb11fcc71b4a3f76030bcc590
Rafal Michalski <royaltm75@gmail.com>
2020-03-20 17:17:33 +0100

    cpu: RegisterPair compact serialization and relaxed deserialization

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -1,6 +1,8 @@
 //! This module contains cpu registers related building blocks.
 #![allow(dead_code)]
-#[cfg(feature = "serde")] use serde::{Serialize, Deserialize};
+#[cfg(feature = "serde")] use serde::{Serialize, Deserialize, Serializer, de::{
+                                            self, Deserializer, Visitor, SeqAccess}};
+#[cfg(feature = "serde")] use std::fmt;
 
 /// The interrupt mode enum.
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
@@ -14,7 +16,6 @@ pub enum InterruptMode {
 
 /// A struct that represents a register pair, that can be treated as a single 16-bit
 /// register or a separate 8-bit (MSB/LSB) registers.
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Clone,Copy,PartialEq,Eq,Default,Hash,Debug)]
 pub struct RegisterPair([u8;2]);
 
@@ -159,9 +160,133 @@ impl RegisterPair {
 
 }
 
+impl From<u16> for RegisterPair {
+    fn from(uint: u16) -> Self {
+        RegisterPair(uint.to_le_bytes())
+    }
+}
+
+impl From<i16> for RegisterPair {
+    fn from(int: i16) -> Self {
+        RegisterPair(int.to_le_bytes())
+    }
+}
+
+impl From<[u8;2]> for RegisterPair {
+    fn from(pair: [u8;2]) -> Self {
+        RegisterPair(pair)
+    }
+}
+
+#[cfg(feature = "serde")]
+impl Serialize for RegisterPair {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_u16(self.get16())
+    }
+}
+
+#[cfg(feature = "serde")]
+struct RegisterPairVisitor;
+
+#[cfg(feature = "serde")]
+impl<'de> Visitor<'de> for RegisterPairVisitor {
+    type Value = RegisterPair;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a signed or unsigned 16-bit integer, a tuple of 8-bit integers or a hex string")
+    }
+
+    fn visit_i16<E: de::Error>(self, value: i16) -> Result<Self::Value, E> {
+        Ok(RegisterPair::from(value))
+    }
+
+    fn visit_u16<E: de::Error>(self, value: u16) -> Result<Self::Value, E> {
+        Ok(RegisterPair::from(value))
+    }
+
+    fn visit_i64<E: de::Error>(self, value: i64) -> Result<Self::Value, E> {
+        if value >= i64::from(std::i16::MIN) && value <= i64::from(std::i16::MAX) {
+            Ok(RegisterPair::from(value as i16))
+        } else {
+            Err(E::custom(format!("RegisterPair out of range: {}", value)))
+        }
+    }
+
+    fn visit_u64<E: de::Error>(self, value: u64) -> Result<Self::Value, E> {
+        if value <= u64::from(std::u16::MAX) {
+            Ok(RegisterPair::from(value as u16))
+        } else {
+            Err(E::custom(format!("RegisterPair out of range: {}", value)))
+        }
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+        where A: SeqAccess<'de>
+    {
+        if let Some(lo) = seq.next_element::<u8>()? {
+            if let Some(hi) = seq.next_element::<u8>()? {
+                if seq.next_element::<u8>()?.is_none() {
+                    return Ok(RegisterPair::from([lo, hi]))
+                }
+            }
+        }
+        Err(de::Error::custom("RegisterPair expects a tuple of 8-bit integers"))
+    }
+
+    fn visit_str<E: de::Error>(self, s: &str) -> Result<Self::Value, E> {
+        let body = if s.starts_with("$") {
+            &s[1..]
+        }
+        else if s.starts_with("0x") {
+            &s[2..]
+        }
+        else {
+            s
+        };
+        let uint = u16::from_str_radix(body, 16).map_err(|_|
+                        de::Error::custom("RegisterPair expects a hexadecimal string"))?;
+        Ok(RegisterPair::from(uint))
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de> Deserialize<'de> for RegisterPair {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_any(RegisterPairVisitor)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[cfg(feature = "serde")]
+    use serde_json;
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn registers_serde() {
+        let regs: RegisterPair = serde_json::from_str("[0,0]").unwrap();
+        assert_eq!(regs, RegisterPair::default());
+        let regs: RegisterPair = serde_json::from_str("0").unwrap();
+        assert_eq!(regs, RegisterPair::default());
+        let mut regs = GeneralRegisters::default();
+        regs.hl.set16(42);
+        regs.bc.set16(776);
+        regs.de.set16(u16::max_value());
+        let serialized = serde_json::to_string(&regs).unwrap();
+        assert_eq!(serialized, r#"{"bc":776,"de":65535,"hl":42}"#);
+        let regs_de: GeneralRegisters = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(regs, regs_de);
+        let regs_de: GeneralRegisters = serde_json::from_str(r#"{"bc":[8,3],"de":-1,"hl":[42,0]}"#).unwrap();
+        assert_eq!(regs, regs_de);
+        let regs_de: GeneralRegisters = serde_json::from_str(r#"{"bc":"308","de":"$ffff","hl":"0x2A"}"#).unwrap();
+        assert_eq!(regs, regs_de);
+    }
 
     #[test]
     fn registers_work() {
